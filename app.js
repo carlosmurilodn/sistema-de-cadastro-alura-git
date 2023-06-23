7cba756ae62c224effda2475db2cfb2a55791a59
carlosmurilodn2 <137318949+carlosmurilodn2@users.noreply.github.com>
2023-06-23 14:29:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ console.log("Rodando o Sistema de Gerenciamento de Jogos")
 console.log("Aplicação se conectadndo ao banco de dados")
 //Mais um console e log de teste:
 console.log("Novo committ")
+//Mais um console e log de teste 2:
+console.log("Novo committ")
